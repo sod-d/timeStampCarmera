cf4bb25be3b21acea71ba5222edd01908b5daa08
uracle <sodud@uracle.co.kr>
2024-05-21 14:43:35 +0900

    Initial Commit

---
+++ timeStamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timeStamp
+//
+//  Created by 유라클 on 2024/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ timeStampUITests/timeStampUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  timeStampUITestsLaunchTests.swift
+//  timeStampUITests
+//
+//  Created by 유라클 on 2024/05/21.
+//
+
+import XCTest
+
+final class timeStampUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
